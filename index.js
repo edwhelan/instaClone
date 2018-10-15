67d9b3412e7203ca610d3ce29fdc67cb1bf188a4
Edward Whelan <vroxing@gmail.com>
2018-10-15 12:09:56 -0400

    bring in header area of IG knockoff

--- index.js
+++ index.js
@@ -15,18 +15,15 @@ const userPictures = [
     `https://images.unsplash.com/photo-1520811607976-6d7812b0ecac?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8baaaa9063852a027216d53f04861841&auto=format&fit=crop&w=1650&q=80`,
     `https://images.unsplash.com/photo-1520811607976-6d7812b0ecac?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8baaaa9063852a027216d53f04861841&auto=format&fit=crop&w=1650&q=80`,
     `https://images.unsplash.com/photo-1520811607976-6d7812b0ecac?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8baaaa9063852a027216d53f04861841&auto=format&fit=crop&w=1650&q=80`,
-    `https://images.unsplash.com/photo-1520811607976-6d7812b0ecac?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8baaaa9063852a027216d53f04861841&auto=format&fit=crop&w=1650&q=80`,
-    `https://images.unsplash.com/photo-1523814054436-e89d40de5d40?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=618dc5d751654b8681465ed042dd282d&auto=format&fit=crop&w=2092&q=80`
+    `https://images.unsplash.com/photo-1523814054436-e89d40de5d40?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=618dc5d751654b8681465ed042dd282d&auto=format&fit=crop&w=2092&q=80`,
 ]
 
-// addEventListener
 
 // populate thumbnails inside divs made from functions below.
 function makeTheThumbnails(arr){
     const target = document.querySelector('[data-container]')
-arr.forEach(function(element) {
+    arr.forEach(function(element) {
     target.appendChild(createThumbnail(element));
-
 });
 }
 
@@ -41,15 +38,14 @@ function createImage(imageURL){
 
     //event listener for generated images to allow user to select one
     //after selection the image is added to bottom area in a 'full size fashion'
-    theImage.addEventListener('click', (event) =>{
+    theImage.addEventListener('click', function(event){
+        //add event listener for click of a image div. Console log the url to ensure it works. 
         const bigPicture = document.querySelector('[data-output]');
-        bigPicture.setAttribute('src',event.target.src);
-        theImage.setAttribute('class', 'selectedImg');
+        // Make Large preview area bellow bassed off of event listener on click
+        bigPicture.setAttribute('src', event.target.src);
+        // theImage.setAttribute('class', 'selectedImg');
     })
-    
-    
     return theImage;
-
 }
 
 //function that generates thumbnail div
@@ -59,10 +55,3 @@ function createThumbnail(url){
     theContainer.appendChild(createImage(url));
     return theContainer;
 }
-
-// Make Large preview area bellow bassed off of event listener on click
-
-//add event listener for click of a image div. Console log the url to ensure it works.
-
-// const targetToBlowUp = document.querySelector('thumbnail-item');
-// targetToBlowUp.addEventListener('click', () => console.log('Yee it work'), false);
